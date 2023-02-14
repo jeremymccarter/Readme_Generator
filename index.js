890df9d0b61092aa30d6863542d4523c453e46b1
dirtnasty jones <jeremymccarter27@gmail.com>
2023-02-14 13:59:59 -0800

    Updated questions for readme, starting working on files to generate, need to put licenses in next

--- index.js
+++ index.js
@@ -1,13 +1,64 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer')
+const fs = require('fs')
+const generateMarkdown = require('./utils/generateMarkdown')
+const path = require('path')
 // TODO: Create an array of questions for user input
-const questions = [];
+
+const questions = [
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'Describe your project/application:',
+        name: 'Description',
+    },
+    {
+        type: 'input',
+        message: 'How to install application:',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Intended Usage of application:',
+        name: 'intendedUsage',
+    },
+    {
+        type: 'input',
+        message: 'License',
+        name: '',
+    },
+
+
+    
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(path.join(process.cwd(), fileName), data, (err) =>
+   err ? console.error(err) : console.log('Success!')
+ );
+
+
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((userInput) => {
+        console.log(userInput)
+        console.log(userInput.jay)
+        writeToFile('README.md', generateMarkdown(userInput))
+
+    })
+
+
+
+
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,9 +12,50 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return ` # ${data.title}
 
-`;
+  Foobar is a Python library for dealing with word pluralization.
+  
+  ##  Installation
+  ${data.installation}
+  
+  Use the package manager [pip](https://pip.pypa.io/en/stable/) to install foobar.
+  
+  bash
+  pip install foobar
+  
+  
+  ## Usage
+  ${data.usage}
+  
+  python
+  import foobar
+  
+  # returns 'words'
+  foobar.pluralize('word')
+  
+  # returns 'geese'
+  foobar.pluralize('goose')
+  
+  # returns 'phenomenon'
+  foobar.singularize('phenomena')
+  
+  
+  ## Contributing
+  
+  Pull requests are welcome. For major changes, please open an issue first
+  to discuss what you would like to change.
+  
+  Please make sure to update tests as appropriate.
+  
+  ## License
+  
+  [MIT](https://choosealicense.com/licenses/mit/)
+  `
+
+
+
+;
 }
 
 module.exports = generateMarkdown;
